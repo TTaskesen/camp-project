3bebcbbe985819b50fb887f827acedde9d0b70d0
turguttaskesen <taskesen@msn.com>
2023-05-23 17:21:18 +0300

    17.21

--- src/layouts/CartSummary.jsx
+++ src/layouts/CartSummary.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { Dropdown } from "semantic-ui-react";
+import { Dropdown, Label } from "semantic-ui-react";
 export default function CartSummary() {
+
+  const {cartItems}=useSelector(state =>state.cart)
+
   return (
     <div>
       <Dropdown item text="Sepetiniz">
         <Dropdown.Menu>
-          <Dropdown.Item>Laptop</Dropdown.Item>
-          <Dropdown.Item>Klavye</Dropdown.Item>
-          <Dropdown.Item>Ekran</Dropdown.Item>
-          <Dropdown.Item>Mouse</Dropdown.Item>
-          <Dropdown.Item>Usb Disk</Dropdown.Item>
-          <Dropdown.Item>Hoparlör</Dropdown.Item>
+          {
+            cartItems.map((cartItem)=>(
+              <Dropdown.Item>
+                {cartItem.product.productName}
+                <Label>
+                  {cartItem.quantity}
+                </Label>
+              </Dropdown.Item>
+            ))
+          }
+          
+          
           <Dropdown.Divider/>
           <Dropdown.Item as={NavLink} to="/cart">Spete Git</Dropdown.Item>
         </Dropdown.Menu>
